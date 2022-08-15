72d2f44e5fee1cb5b858fe25c44eb787e6abb67c
ummadiviany <ummadi.vinay2000@gmail.com>
2022-08-16 02:30:37 +0530

    swap

--- README.md
+++ README.md
@@ -7,4 +7,8 @@ This reposritry contains code snippets for CS60013 : Programming and Data Struct
     3. Operators
     4. Arithmetic Operators
     5. Printing in Python
+    6. Input in Python
+    7. Type Conversion
+    8. Calculator [In-Class Exercise]
+    9. Swapping Variables
 
---
+++ basics/9_swap.py
@@ -0,0 +1,24 @@
+# /**
+#  * @author Vinay Ummadi
+#  * @email ummadi.vinay2000@gmail.com
+#  * @create date 2022-08-16 02:17:17
+#  * @modify date 2022-08-16 02:17:17
+#  * @desc [description]
+#  */
+# Actual code starts below this line
+# -----------------------------------------------------------
+
+# Swapping of two variables
+
+print(f"\n-------------------Swapping of two variables-------------------\n")
+
+a = 10
+b = 20
+
+print(f"Before swapping, a = {a} and b = {b}")
+
+# Swap the values of a and b
+a, b = b, a
+
+print(f"After swapping, a = {a} and b = {b}")
+
